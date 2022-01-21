1ab3f0ea39e6c4e7e2bc34f02d438f3fddbefa52
RafaelTrust <rafaelimaferreira@gmail.com>
2022-01-21 03:48:30 -0300

    tentando fazer rodar api

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
-const PORT = process.env.PORT || 5050;
-
-app.listen(PORT, () => console.log(`Servidor esta rodando na porta ${PORT}`))
 
 app.use(cors())
 
